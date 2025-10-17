adb4c962ba6bb59e4c5dc523610468826c885386
SaritoHokage <57367110+SaritoHokage@users.noreply.github.com>
2025-10-17 23:09:52 +0700

    urls.py

--- config/config/urls.py
+++ config/config/urls.py
@@ -5,13 +5,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # UI (лежит внутри пакета config)
     path('', include('config.ui.urls')),
 
-    # API (лежит рядом с config)
     path('api/', include('api.urls')),
 
-    # OpenAPI/Swagger
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
